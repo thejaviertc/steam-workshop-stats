f948f4dab9bc335ed102b4c9935e598a7366d9a2
thejaviertc <javiertcjobs@gmail.com>
2020-08-21 23:29:08 +0200

    Make Form that get all the links necesaries Fixes #2

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,16 @@ const express = require('express');
 const router = express.Router();
 
 // Local Dependencies
-const { scrap } = require("../controllers/scrap");
+const scrap = require("../controllers/scrap");
 
 // Index
-router.get("/", scrap);
+router.get('/', (req, res) => {
+    res.render('index');
+});
+
+router.post("/workshop", scrap.getUrl);
+
+// Workshop
+router.get("/workshop", scrap.getInfo);
 
 module.exports = router;
