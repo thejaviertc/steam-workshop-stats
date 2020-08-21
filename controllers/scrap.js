2ad7b6a5994fe9d4b452ae2fe923f2dca7de34cc
thejaviertc <javiertcjobs@gmail.com>
2020-08-21 19:08:25 +0200

    Remake all Scap System Fixes #1

--- controllers/scrap.js
+++ controllers/scrap.js
@@ -1,30 +1,98 @@
 // Scrap Module
-const request = require("request");
-const cheerio = require("cheerio");
+const request = require('request-promise');
+const $ = require("cheerio");
+
+const url = [
+    "https://steamcommunity.com/sharedfiles/filedetails/?id=2143878031",
+    "https://steamcommunity.com/sharedfiles/filedetails/?id=2072552206",
+    "https://steamcommunity.com/sharedfiles/filedetails/?id=2064163638",
+    "https://steamcommunity.com/sharedfiles/filedetails/?id=1821273859",
+    "https://steamcommunity.com/sharedfiles/filedetails/?id=1707130373",
+    "https://steamcommunity.com/sharedfiles/filedetails/?id=1706110647",
+    "https://steamcommunity.com/sharedfiles/filedetails/?id=1706097473",
+    "https://steamcommunity.com/sharedfiles/filedetails/?id=1591399382",
+    "https://steamcommunity.com/sharedfiles/filedetails/?id=1586644047",
+    "https://steamcommunity.com/sharedfiles/filedetails/?id=1586640869",
+    "https://steamcommunity.com/sharedfiles/filedetails/?id=1586636847",
+    "https://steamcommunity.com/sharedfiles/filedetails/?id=1586630442",
+    "https://steamcommunity.com/sharedfiles/filedetails/?id=1391291513",
+    "https://steamcommunity.com/sharedfiles/filedetails/?id=1384845390",
+    "https://steamcommunity.com/sharedfiles/filedetails/?id=1344898873",
+    "https://steamcommunity.com/sharedfiles/filedetails/?id=1343453090",
+    "https://steamcommunity.com/sharedfiles/filedetails/?id=1129397431",
+    "https://steamcommunity.com/sharedfiles/filedetails/?id=951229129",
+    "https://steamcommunity.com/sharedfiles/filedetails/?id=951100143",
+    "https://steamcommunity.com/sharedfiles/filedetails/?id=951097085",
+    "https://steamcommunity.com/sharedfiles/filedetails/?id=856663574",
+    "https://steamcommunity.com/sharedfiles/filedetails/?id=856264732",
+    "https://steamcommunity.com/sharedfiles/filedetails/?id=856202832",
+    "https://steamcommunity.com/sharedfiles/filedetails/?id=851871971",
+    "https://steamcommunity.com/sharedfiles/filedetails/?id=851569336",
+    "https://steamcommunity.com/sharedfiles/filedetails/?id=843988439",
+    "https://steamcommunity.com/sharedfiles/filedetails/?id=836933262",
+    "https://steamcommunity.com/sharedfiles/filedetails/?id=835525732",
+    "https://steamcommunity.com/sharedfiles/filedetails/?id=820160895",
+    "https://steamcommunity.com/sharedfiles/filedetails/?id=819453573",
+    "https://steamcommunity.com/sharedfiles/filedetails/?id=766496731",
+    "https://steamcommunity.com/sharedfiles/filedetails/?id=766121584",
+    "https://steamcommunity.com/sharedfiles/filedetails/?id=766115310",
+    "https://steamcommunity.com/sharedfiles/filedetails/?id=746885345",
+    "https://steamcommunity.com/sharedfiles/filedetails/?id=746402915"
+];
 
 module.exports = {
     scrap: function (req, res) {
-        request("https://steamcommunity.com/sharedfiles/filedetails/?id=2143878031", (error, response, html) => {
-            if (!error && response.statusCode == 200) {
-                const $ = cheerio.load(html);
-
-                const siteHeading = $(".stats_table");
-
-                let output = siteHeading
-                    .find("td")
-                    .text()
-                let newOutput = output.match(/[a-zA-Z]+|[0-9]+/g)
-
-                var visitors = newOutput[0]
-                let suscribers = newOutput[3]
-                let favorites = newOutput[6]
-
-                res.render('index', {
-                    visitors: visitors,
-                    suscribers: suscribers,
-                    favorites: favorites
+        let results = [];
+        url.forEach(function (url) {
+            request(url)
+                .then(html => {
+                    results.push({
+                        name: $('.workshopItemTitle', html).text(),
+                        stars: $('.fileRatingDetails img', html).attr("src"),
+                        image: $('#previewImageMain', html).attr("src").split("?imw").shift(),
+                        viewers: $('tbody tr:nth-child(1) td:nth-child(1)', html).text(),
+                        subs: $('tbody tr:nth-child(2) td:nth-child(1)', html).text(),
+                        favs: $('tbody tr:nth-child(3) td:nth-child(1)', html).text()
+                    })
+                })
+                .catch(error => {
+                    console.log(error);
                 });
+        });
+        setTimeout(() => {
+            let viewersArrayString = [];
+            let subsArrayString = [];
+            let favsArrayString = [];
+            let i;
+
+            for (i = 0; i < results.length; i++) {
+                viewersArrayString.push(
+                    results[i].viewers.replace(",", '')
+                )
             }
-        })
+
+            for (i = 0; i < results.length; i++) {
+                subsArrayString.push(
+                    results[i].subs.replace(",", '')
+                )
+            }
+
+            for (i = 0; i < results.length; i++) {
+                favsArrayString.push(
+                    results[i].favs.replace(",", '')
+                )
+            }
+
+            let viewersArray = viewersArrayString.map(Number).reduce((a, b) => a + b, 0);
+            let subsArray = subsArrayString.map(Number).reduce((a, b) => a + b, 0);
+            let favsArray = favsArrayString.map(Number).reduce((a, b) => a + b, 0);
+
+            res.render('index', {
+                array: results,
+                viewers: viewersArray,
+                subs: subsArray,
+                favs: favsArray
+            });
+        }, 7000);
     }
 }
