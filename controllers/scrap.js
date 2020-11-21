074a15094ee4b8103b7de8223b0e5a8c34ec8548
thejaviertc <javiertcjobs@gmail.com>
2020-11-22 00:46:54 +0100

    Made Optimized

--- controllers/scrap.js
+++ controllers/scrap.js
@@ -2,134 +2,86 @@
 const scrap = {};
 const request = require('request-promise');
 
-scrap.getAddonList = (req, res) => {
-    const { steamId, gameId } = req.body;
-    let numberAddons;
-    let ids = [];
-
-    scrap.getNumberAddons(steamId, gameId, numberAddons);
-    console.log(numberAddons + "1");
-    scrap.getUserName(steamId);
-    scrap.getIDS(steamId, gameId, numberAddons, ids);
-}
-
-scrap.getNumberAddons = (steamId, gameId, numberAddons) => {
-    request.get("https://api.steampowered.com/IPublishedFileService/GetUserFiles/v1/?key=0E59F59542C9B1C2E33DFB89210B588F&steamid=" + steamId + "&appid=" + gameId + "&totalonly=1", (error, resp, body) => {
-        const data = JSON.parse(body);
-        numberAddons = data["response"].total;
-        console.log(numberAddons);
-    })
-}
-
-scrap.getUserName = (steamId) => {
-    request.get("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=0E59F59542C9B1C2E33DFB89210B588F&steamids=" + steamId, (error, resp, body) => {
-        const data = JSON.parse(body);
-        const userName = data["response"].players[0].personaname;
-        return userName;
-    })
-}
-
-scrap.getIDS = (steamId, gameId, quantity, ids) => {
-    request.get("https://api.steampowered.com/IPublishedFileService/GetUserFiles/v1/?key=0E59F59542C9B1C2E33DFB89210B588F&steamid=" + steamId + "&appid=" + gameId + "&page=1&numperpage=" + quantity + "&sortmethod=date&ids_only=1", (error, resp, body) => {
-        const data = JSON.parse(body);
-
-        for (let i = 0; i < quantity; i++) {
-            ids.push(data["response"].publishedfiledetails[i].publishedfileid);
-            console.log("Algo");
-        }
-    })
-}
-
-// scrap.getInfo = (req, res) => {
-//     let idList = [];
-
-//     // Get All User Published Addons
-//     request.get("https://api.steampowered.com/IPublishedFileService/GetUserFiles/v1/?key=0E59F59542C9B1C2E33DFB89210B588F&steamid=" + steamId + "&appid=" + gameId + "&page=1&numperpage=" + quantity + "&sortmethod=date", (error, resp, body) => {
-//         const data = JSON.parse(body);
-
-//         // Get the IDs of all of his addons
-//         for (let i = 0; i < quantity; i++) {
-//             idList.push(
-//                 data["response"].publishedfiledetails[i].publishedfileid
-//             )
-//         }
-
-//     })
-//     setTimeout(() => {
-//         // Data for API
-//         let requestData = {
-//             "format": 'json',
-//             "itemcount": idList.length
-//         }
-
-//         // Add the IDs to the requestData
-//         for (let i = 0; i < idList.length; i++) {
-//             requestData['publishedfileids[' + i + ']'] = idList[i];
-//         }
-
-//         // Get All Addon Stats
-//         request.post("https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1/", { form: requestData }, (error, resp, body) => {
-//             const data = JSON.parse(body);
-
-//             let results = [];
-
-//             // Add the stats to results
-//             for (let i = 0; i < idList.length; i++) {
-//                 results.push({
-//                     image: data["response"].publishedfiledetails[i].preview_url,
-//                     title: data["response"].publishedfiledetails[i].title,
-//                     subs: data["response"].publishedfiledetails[i].subscriptions,
-//                     favs: data["response"].publishedfiledetails[i].favorited,
-//                     lifeSubs: data["response"].publishedfiledetails[i].lifetime_subscriptions,
-//                     lifeFavs: data["response"].publishedfiledetails[i].lifetime_favorited,
-//                     views: data["response"].publishedfiledetails[i].views
-//                 })
-//             }
-
-//             let subsArray = []
-//             let favsArray = []
-//             let lifeSubsArray = []
-//             let lifeFavsArray = []
-//             let viewersArray = []
-
-//             // Add the specific stats to each array for sum it
-//             for (let i = 0; i < idList.length; i++) {
-//                 subsArray.push(
-//                     results[i].subs
-//                 )
-//                 favsArray.push(
-//                     results[i].favs
-//                 )
-//                 lifeSubsArray.push(
-//                     results[i].lifeSubs
-//                 )
-//                 lifeFavsArray.push(
-//                     results[i].lifeFavs
-//                 )
-//                 viewersArray.push(
-//                     results[i].views
-//                 )
-//             }
-
-//             // Sum all arrays
-//             let totalSubs = subsArray.map(Number).reduce((a, b) => a + b, 0);
-//             let totalFavs = favsArray.map(Number).reduce((a, b) => a + b, 0);
-//             let totalLifeSubs = lifeSubsArray.map(Number).reduce((a, b) => a + b, 0);
-//             let totalLifeFavs = lifeFavsArray.map(Number).reduce((a, b) => a + b, 0);
-//             let totalViewers = viewersArray.map(Number).reduce((a, b) => a + b, 0);
-
-//             res.render("workshop", {
-//                 user: userName,
-//                 array: results,
-//                 subs: totalSubs,
-//                 favs: totalFavs,
-//                 lifeSubs: totalLifeSubs,
-//                 lifeFavs: totalLifeFavs,
-//                 viewers: totalViewers
-//             });
-//         })
-//     }, 2500);
-// }
-
 // Module Export
-module.exports = scrap;
+module.exports = function () {
+    this.getAddonList = function (req, res) {
+        // Required Data
+        const { steamId, gameId } = req.body;
+
+        // getNumberAddons
+        request.get("https://api.steampowered.com/IPublishedFileService/GetUserFiles/v1/?key=0E59F59542C9B1C2E33DFB89210B588F&steamid=" + steamId + "&appid=" + gameId + "&totalonly=1", (error, resp, body) => {
+            const data = JSON.parse(body);
+            const numberAddons = data["response"].total;
+
+            // getUserName
+            request.get("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=0E59F59542C9B1C2E33DFB89210B588F&steamids=" + steamId, (error, resp, body) => {
+                const data = JSON.parse(body);
+                const userName = data["response"].players[0].personaname;
+
+                // getAddonsIds
+                request.get("https://api.steampowered.com/IPublishedFileService/GetUserFiles/v1/?key=0E59F59542C9B1C2E33DFB89210B588F&steamid=" + steamId + "&appid=" + gameId + "&numperpage=" + numberAddons + "&ids_only=1&sortmethod=date", (error, resp, body) => {
+                    const data = JSON.parse(body);
+                    const idsList = [];
+
+                    // Add all ids to the array
+                    for (let i = 0; i < numberAddons; i++)
+                        idsList.push(data["response"].publishedfiledetails[i].publishedfileid);
+
+                    // Data for the last request
+                    let requestData = {
+                        "format": 'json',
+                        "itemcount": idsList.length
+                    }
+
+                    // Add the IDs to the requestData
+                    for (let i = 0; i < idsList.length; i++) {
+                        requestData['publishedfileids[' + i + ']'] = idsList[i];
+                    }
+
+                    // Get All Addon Stats
+                    request.post("https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1/", { form: requestData }, (error, resp, body) => {
+                        const data = JSON.parse(body);
+
+                        let finalResults = [];
+
+                        // Add the stats to finalResults
+                        for (let i = 0; i < idsList.length; i++) {
+                            finalResults.push({
+                                image: data["response"].publishedfiledetails[i].preview_url,
+                                title: data["response"].publishedfiledetails[i].title,
+                                subs: data["response"].publishedfiledetails[i].subscriptions,
+                                favs: data["response"].publishedfiledetails[i].favorited,
+                                lifeSubs: data["response"].publishedfiledetails[i].lifetime_subscriptions,
+                                lifeFavs: data["response"].publishedfiledetails[i].lifetime_favorited,
+                                views: data["response"].publishedfiledetails[i].views
+                            })
+                        }
+
+                        // Last Variables
+                        let totalSubs = 0, totalFavs = 0, totalLifeSubs = 0, totalLifeFavs = 0, totalViewers = 0;
+
+                        // Get All Totals
+                        for (let i = 0; i < idsList.length; i++) {
+                            totalSubs += finalResults[i].subs;
+                            totalFavs += finalResults[i].favs;
+                            totalLifeSubs += finalResults[i].lifeSubs;
+                            totalLifeFavs += finalResults[i].lifeFavs;
+                            totalViewers += finalResults[i].views;
+                        }
+
+                        // Render all the data requested
+                        res.render("workshop", {
+                            user: userName,
+                            array: finalResults,
+                            subs: totalSubs,
+                            favs: totalFavs,
+                            lifeSubs: totalLifeSubs,
+                            lifeFavs: totalLifeFavs,
+                            viewers: totalViewers
+                        });
+                    })
+                })
+            })
+        })
+    };
+}
--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,16 @@ const express = require('express');
 const router = express.Router();
 
 // Controllers
-const scrap = require("../controllers/scrap");
+require("../controllers/scrap")();
 
 // Index Route
 router.get('/', (req, res) => {
     res.render('index');
 });
 
-router.post("/workshop", scrap.getAddonList);
+router.post("/workshop", (req, res) => {
+    getAddonList(req, res);
+});
 
 // Workshop Route
 router.get("/workshop");
