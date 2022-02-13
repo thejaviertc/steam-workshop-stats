82b01de774dbce6b77fb66b5555b308a89a5ca39
thejaviertc <javier.toribiocouz@gmail.com>
2022-02-13 15:38:16 +0100

    Fixed fontawesome bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // Dependencies
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 
 // Other
 import './App.scss';
