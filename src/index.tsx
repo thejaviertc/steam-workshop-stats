930bed86f3dc8e6b3ed030038052904a27774981
thejaviertc <javier.toribiocouz@gmail.com>
2022-04-09 23:01:50 +0200

    Upgraded to React v18

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
-import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 reportWebVitals();
