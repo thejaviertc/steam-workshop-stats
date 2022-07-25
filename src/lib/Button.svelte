e07a33416a8c1846964f0c5076f551a2ae58b0ff
thejaviertc <javier.toribiocouz@gmail.com>
2022-07-25 21:59:18 +0200

    v1.4.0

---
+++ src/lib/Button.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+
+	import Fa from "svelte-fa";
+
+	export let faIcon: IconDefinition;
+	export let link: string;
+	export let text: string;
+</script>
+
+<a class="btn btn-secondary mx-2" href={link} role="button">
+	<Fa icon={faIcon} />
+	{text}
+</a>
