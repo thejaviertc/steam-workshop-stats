de978be0fd5c2ab9762258b9d2b36cc1c2ee82e1
thejaviertc <javier.toribiocouz@gmail.com>
2022-12-18 23:44:36 +0100

    Implement Unit Tests

---
+++ tests/Addon.test.ts
@@ -0,0 +1,10 @@
+import Addon from "$lib/Addon.svelte";
+import { render } from "@testing-library/svelte";
+import { describe, it } from "vitest";
+import "../src/i18n";
+
+describe("Addon Component", () => {
+	it("Renders", () => {
+		render(Addon, { title: "Addon", image: "image", url: "https://addon.com", viewers: 50, subs: 5, lifeSubs: 7, favs: 2, lifeFavs: 3, likes: 5, dislikes: 2 });
+	});
+});
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,15 @@ import { sveltekit } from "@sveltejs/kit/vite";
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
+	test: {
+		include: ["tests/**"],
+		environment: "jsdom",
+		reporter: "verbose",
+		coverage: {
+			all: true,
+			include: ["src/**/*.{js,ts,svelte}"],
+		}
+	}
 };
 
 export default config;
