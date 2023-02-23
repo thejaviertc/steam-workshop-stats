1df3af8daaa740b38e3951bbc27c2a19c54362fd
thejaviertc <javier.toribiocouz@gmail.com>
2023-02-23 23:25:43 +0100

    Add DaisyUI

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,5 +4,18 @@ module.exports = {
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [require("daisyui")],
+	daisyui: {
+		themes: [
+			{
+				steam: {
+					"primary": "#1b2838",
+					"secondary": "#171a21",
+					"accent": "#38bdf8",
+					"neutral": "#3d4451",
+					"base-100": "#ffffff",
+				},
+			},
+		],
+	},
 };
