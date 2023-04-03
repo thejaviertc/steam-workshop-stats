a8c59b3f71cad8581f691cf14f1ddb19c0cf4fb7
thejaviertc <javier.toribiocouz@gmail.com>
2023-04-03 16:57:31 +0200

    Prevent implicit Any

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,13 +9,14 @@ module.exports = {
 		themes: [
 			{
 				steam: {
-					"primary": "#1b2838",
-					"secondary": "#171a21",
-					"accent": "#38bdf8",
-					"neutral": "#3d4451",
+					"primary": "#1B2838",
+					"secondary": "#171A21",
+					"accent": "#38BDf8",
+					"neutral": "#3D4451",
 					"base-100": "#ffffff",
 				},
 			},
 		],
 	},
+	safelist: ["bg-primary", "bg-secondary", "btn-primary", "btn-secondary"],
 };
