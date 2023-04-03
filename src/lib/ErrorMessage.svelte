7995e8caf1269144f1b6c60644627a7e2b81caff
thejaviertc <javier.toribiocouz@gmail.com>
2023-04-04 00:12:02 +0200

    Redesign Notification

--- src/lib/ErrorMessage.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	export let message: string;
-</script>
-
-<div class="container mx-auto px-6">
-	<div class="bg-red-600 px-2 py-6 text-center rounded-xl">
-		{message}
-	</div>
-</div>
---
+++ src/lib/Notification.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import type { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+	import Fa from "svelte-fa";
+
+	export let faIcon: IconDefinition;
+</script>
+
+<div class={`mx-auto mt-8 py-8 w-3/4 text-center rounded-xl ${$$props.class}`}>
+	<Fa icon={faIcon} />
+	<slot />
+</div>
