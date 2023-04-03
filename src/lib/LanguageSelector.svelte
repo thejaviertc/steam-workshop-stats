0a149fb3d01d9ea7c01c4d778161e6bc9741e8fb
thejaviertc <javier.toribiocouz@gmail.com>
2023-04-03 17:41:46 +0200

    Implement LanguageSelector

---
+++ src/lib/LanguageSelector.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import { faLanguage } from "@fortawesome/free-solid-svg-icons";
+	import Fa from "svelte-fa";
+	import { locale, _ } from "svelte-i18n";
+
+	/**
+	 * Changes the website language to the selected
+	 * @param e
+	 */
+	function changeLanguage(e: any) {
+		e.preventDefault();
+		locale.set(e.target.value);
+	}
+</script>
+
+<div class="dropdown dropdown-end">
+	<button tabindex="-1" class="btn btn-secondary normal-case text-sm md:text-base text-gray-100"
+		><Fa class="mr-2" icon={faLanguage} />Language</button
+	>
+	<ul
+		tabindex="-1"
+		class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-secondary rounded-box w-52"
+	>
+		<li>
+			<button on:click={changeLanguage} value="en" class="text-lg"
+				>🇬🇧 {$_("language", { locale: "en" })}</button
+			>
+		</li>
+		<li>
+			<button on:click={changeLanguage} value="es" class="text-lg"
+				>🇪🇸 {$_("language", { locale: "es" })}</button
+			>
+		</li>
+		<li>
+			<button on:click={changeLanguage} value="th" class="text-lg"
+				>🇹🇭 {$_("language", { locale: "th" })}</button
+			>
+		</li>
+		<li>
+			<button class="text-lg">{$_("buttons.helpTranslating")}</button>
+		</li>
+	</ul>
+</div>
