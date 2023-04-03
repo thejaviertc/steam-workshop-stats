337eb7b8f507bd4db1523b8303cf5b2cd7753201
thejaviertc <javier.toribiocouz@gmail.com>
2023-04-03 17:44:54 +0200

    Redesign Error Page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -8,11 +8,7 @@
 	<title>{$page.status}</title>
 </svelte:head>
 
-<div class="bg-slate-800 min-h-screen flex justify-center items-center">
-	<div>
-		<h2>Something Happened - {$page.status}</h2>
-		<div class="flex justify-center my-6">
-			<Button faIcon={faBackward} link="/" text="Return Back" />
-		</div>
-	</div>
-</div>
+<section class="min-h-screen flex flex-col justify-center items-center text-center">
+	<h2 class="mb-6">Something Happened - {$page.status}</h2>
+	<Button class="btn-accent" faIcon={faBackward} link="/">Return Home</Button>
+</section>
