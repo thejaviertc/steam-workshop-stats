9e2a1aa34827e0586bea4982e7b88e7dd387ce50
thejaviertc <javier.toribiocouz@gmail.com>
2023-04-10 20:18:23 +0200

    Add i18n

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,6 +2,7 @@
 	import { page } from "$app/stores";
 	import Button from "$lib/Button.svelte";
 	import { faBackward } from "@fortawesome/free-solid-svg-icons";
+	import { _ } from "svelte-i18n";
 </script>
 
 <svelte:head>
@@ -9,6 +10,8 @@
 </svelte:head>
 
 <section class="min-h-screen flex flex-col justify-center items-center text-center">
-	<h2 class="mb-6">Something Happened - {$page.status}</h2>
-	<Button class="btn-accent" faIcon={faBackward} link="/">Return Home</Button>
+	<h2 class="mb-6">
+		{$_("misc.httpError")} - {$page.status}
+	</h2>
+	<Button class="btn-accent" faIcon={faBackward} link="/">{$_("actions.returnHome")}</Button>
 </section>
