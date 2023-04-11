002b28a12ddac8523027c5332a090d98d53f2ee8
thejaviertc <javier.toribiocouz@gmail.com>
2023-04-11 20:00:30 +0200

    Add Stat component

---
+++ src/lib/Stat.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+	import Fa from "svelte-fa";
+
+	export let title: string;
+	export let faIcon: IconDefinition;
+	export let value: number;
+</script>
+
+<div class="stat">
+	<div class="stat-title">{title}</div>
+	<div class="stat-value">
+		<Fa icon={faIcon} />
+		{value.toLocaleString()}
+	</div>
+</div>
