06138371741fb65ef7081ad927dc0dfeb5c28a41
thejaviertc <javier.toribiocouz@gmail.com>
2023-08-02 19:17:19 +0200

    Fix disabled button not showing text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,7 +13,7 @@
 	<h1>{$_("title")}</h1>
 	<h4 class="my-4">{$_("description")}</h4>
 	<div class="my-6">
-		<Button class="btn-disabled mb-4 sm:mb-0" faIcon={faChartLine} link={base}
+		<Button class="btn btn-neutral mb-4 sm:mb-0" faIcon={faChartLine} link={base}
 			>{$_("actions.trackMyStats")}</Button
 		>
 		<!-- TODO: Add btn-outline -->
