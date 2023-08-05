bfbf8e76aba61b7bad7abf66cbe6a3021dd75528
thejaviertc <javier.toribiocouz@gmail.com>
2023-08-05 22:53:26 +0200

    Make test cover 100%

--- src/components/Addon.test.ts
+++ src/components/Addon.test.ts
@@ -1,10 +1,10 @@
 import { render } from "@testing-library/svelte";
 import { describe, it } from "vitest";
-// import "../i18n";
+import "../i18n";
 import Addon from "./Addon.svelte";
 
 describe("Addon Component", () => {
-	it("Renders", () => {
+	it("Renders an addon with no stars", () => {
 		render(Addon, {
 			id: 1,
 			title: "Addon",
@@ -15,6 +15,20 @@ describe("Addon Component", () => {
 			likes: 5,
 			dislikes: 2,
 			stars: 0,
+		});
+	});
+
+	it("Renders an addon with stars", () => {
+		render(Addon, {
+			id: 2,
+			title: "Addon 2",
+			image: "image",
+			views: 125,
+			subscribers: 50,
+			favorites: 10,
+			likes: 25,
+			dislikes: 5,
+			stars: 3,
 		});
 	});
 });
