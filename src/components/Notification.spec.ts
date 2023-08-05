ee35f81e51fc0c2254cb3e2746b15b83d6497779
thejaviertc <javier.toribiocouz@gmail.com>
2023-08-05 23:09:52 +0200

    Fix component props

--- src/components/Notification.spec.ts
+++ src/components/Notification.spec.ts
@@ -1,9 +1,10 @@
+import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
 import { render } from "@testing-library/svelte";
 import { describe, it } from "vitest";
 import Notification from "./Notification.svelte";
 
 describe("Notification Component", () => {
 	it("Renders", () => {
-		render(Notification, { message: "Error Message" });
+		render(Notification, { faIcon: faCircleInfo });
 	});
 });
