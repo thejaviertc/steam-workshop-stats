fabb97c6de3f6920ec262165e876df66ccb0f173
thejaviertc <javier.toribiocouz@gmail.com>
2023-08-06 23:11:39 +0200

    Add UserStats tests

---
+++ src/components/UserStats.spec.ts
@@ -0,0 +1,45 @@
+import { render } from "@testing-library/svelte";
+import { describe, it } from "vitest";
+import "../i18n";
+import UserStats from "./UserStats.svelte";
+
+describe("UserStats Component", () => {
+	it("Renders", () => {
+		render(UserStats, {
+			steamUser: {
+				steamId: "STEAMID64",
+				username: "javiertc",
+				profileImage: "image",
+				views: 500,
+				subscribers: 150,
+				favorites: 20,
+				likes: 50,
+				dislikes: 10,
+				addons: [
+					{
+						id: 1,
+						title: "Addon",
+						image: "image",
+						views: 250,
+						subscribers: 75,
+						favorites: 10,
+						likes: 25,
+						dislikes: 5,
+						stars: 2,
+					},
+					{
+						id: 2,
+						title: "Addon 2",
+						image: "image",
+						views: 250,
+						subscribers: 75,
+						favorites: 10,
+						likes: 25,
+						dislikes: 5,
+						stars: 4,
+					},
+				],
+			},
+		});
+	});
+});
