3b14e9728bee415a0e359c88080e517c77e01f9d
thejaviertc <javier.toribiocouz@gmail.com>
2024-02-04 21:05:05 +0100

    Test: Add missing tests

---
+++ src/components/Graph.spec.ts
@@ -0,0 +1,35 @@
+import { render } from "@testing-library/svelte";
+import { describe, it } from "vitest";
+import "../i18n";
+
+import Graph from "./Graph.svelte";
+
+describe("Graph Component", () => {
+	it("Renders", () => {
+		render(Graph, {
+			steamUser: {
+				steamId: "STEAMID64",
+				username: "javiertc",
+				profileImage: "image",
+				views: 125,
+				subscribers: 121,
+				favorites: 15,
+				likes: 25,
+				dislikes: 5,
+				addons: [
+					{
+						id: 1,
+						title: "Addon",
+						image: "image",
+						views: 125,
+						subscribers: 121,
+						favorites: 15,
+						likes: 25,
+						dislikes: 5,
+						stars: 4,
+					},
+				],
+			},
+		});
+	});
+});
--- src/components/Notification.spec.ts
+++ src/components/Notification.spec.ts
@@ -1,10 +1,11 @@
+import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
 import { render } from "@testing-library/svelte";
 import { describe, it } from "vitest";
 
-import Notification from "$components/Notification.svelte";
+import Notification from "./Notification.svelte";
 
 describe("Notification Component", () => {
 	it("Renders", () => {
-		render(Notification, { message: "Error Message" });
+		render(Notification, { faIcon: faCircleInfo });
 	});
 });
