cbc59a1f97b86be47d2df8b0b7639c404fa6b091
thejaviertc <javier.toribiocouz@gmail.com>
2024-02-04 19:35:36 +0100

    Test: Move tests near the files that tests

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { sveltekit } from "@sveltejs/kit/vite";
 const config = {
 	plugins: [sveltekit()],
 	test: {
-		include: ["src/tests/**"],
 		environment: "jsdom",
 		reporter: "verbose",
 		coverage: {
