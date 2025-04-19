f99b83d393c3a9b0fea199f6328215f31a59523b
thejaviertc <javier.toribiocouz@gmail.com>
2025-04-19 23:28:21 +0200

    chore: Remove unit tests.

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,6 @@ import { sveltekit } from "@sveltejs/kit/vite";
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
-	test: {
-		environment: "jsdom",
-		reporter: "verbose",
-		coverage: {
-			all: true,
-			include: ["src/**/*.{js,ts,svelte}"],
-		},
-	},
 };
 
 export default config;
