49005ca53811078841c402e6a292ed334226f410
thejaviertc <javier.toribiocouz@gmail.com>
2025-08-20 19:43:34 +0200

    feat: Sort order.

--- src/components/UserStats.svelte
+++ src/components/UserStats.svelte
@@ -2,48 +2,54 @@
 	import {
 		faCircleInfo,
 		faEye,
+		faSortDown,
+		faSortUp,
 		faStar,
 		faThumbsDown,
 		faThumbsUp,
-		faUser,
 		faTrophy,
+		faUser,
 	} from "@fortawesome/free-solid-svg-icons";
+	import Fa from "svelte-fa";
 	import { _ } from "svelte-i18n";
 
 	import Addon from "$components/Addon.svelte";
 	import Graph from "$components/Graph.svelte";
 	import StatTitle from "$components/StatTitle.svelte";
 	import type { ISteamUser } from "$lib/ISteamUser";
-	import Notification from "./Notification.svelte";
 	import SortingType from "$lib/SortingType";
+	import Notification from "./Notification.svelte";
 
 	export let steamUser: ISteamUser;
 	let tab: string = "addons";
 
 	// Sorting state
 	let sortType: SortingType = SortingType.PublishDate;
+	let sortDescendingOrder: boolean = true;
 
 	// Computed visible addons based on sort type
 	$: visibleAddons = [...steamUser.addons].sort((a, b) => {
 		switch (sortType as SortingType) {
 			case SortingType.Views:
-				return b.views - a.views;
+				return sortDescendingOrder ? b.views - a.views : a.views - b.views;
 
 			case SortingType.Suscribers:
-				return b.subscribers - a.subscribers;
+				return sortDescendingOrder
+					? b.subscribers - a.subscribers
+					: a.subscribers - b.subscribers;
 
 			case SortingType.Favorites:
-				return b.favorites - a.favorites;
+				return sortDescendingOrder ? b.favorites - a.favorites : a.favorites - b.favorites;
 
 			case SortingType.Likes:
-				return b.likes - a.likes;
+				return sortDescendingOrder ? b.likes - a.likes : a.likes - b.likes;
 
 			case SortingType.Dislikes:
-				return b.dislikes - a.dislikes;
+				return sortDescendingOrder ? b.dislikes - a.dislikes : a.dislikes - b.dislikes;
 
 			case SortingType.PublishDate:
 			default:
-				return 0;
+				return sortDescendingOrder ? -1 : 1;
 		}
 	});
 
@@ -99,17 +105,29 @@
 			</button>
 		</div>
 		{#if tab === "addons"}
-			<fieldset class="fieldset mb-8">
-				<legend class="fieldset-legend text-lg">Sort by:</legend>
-				<select bind:value={sortType} class="select bg-primary border-accent">
-					<option value={SortingType.PublishDate} selected>Publish Date</option>
-					<option value={SortingType.Views}>{$_("stats.views")}</option>
-					<option value={SortingType.Suscribers}>{$_("stats.subscribers")}</option>
-					<option value={SortingType.Favorites}>{$_("stats.favorites")}</option>
-					<option value={SortingType.Likes}>{$_("stats.likes")}</option>
-					<option value={SortingType.Dislikes}>{$_("stats.dislikes")}</option>
-				</select>
-			</fieldset>
+			<div class="flex">
+				<fieldset class="fieldset mx-2 mb-8">
+					<legend class="fieldset-legend text-lg">Sort by:</legend>
+					<select bind:value={sortType} class="select bg-primary border-accent">
+						<option value={SortingType.PublishDate} selected>Publish Date</option>
+						<option value={SortingType.Views}>{$_("stats.views")}</option>
+						<option value={SortingType.Suscribers}>{$_("stats.subscribers")}</option>
+						<option value={SortingType.Favorites}>{$_("stats.favorites")}</option>
+						<option value={SortingType.Likes}>{$_("stats.likes")}</option>
+						<option value={SortingType.Dislikes}>{$_("stats.dislikes")}</option>
+					</select>
+				</fieldset>
+
+				<fieldset class="fieldset mx-2 mb-8">
+					<legend class="fieldset-legend text-lg">Sort order:</legend>
+					<label class="swap text-2xl">
+						<input type="checkbox" bind:checked={sortDescendingOrder} />
+
+						<Fa icon={faSortDown} class="swap-on fill-current" />
+						<Fa icon={faSortUp} class="swap-off fill-current" />
+					</label>
+				</fieldset>
+			</div>
 
 			<h2 class="mb-8">
 				{$_("stats.addonsOf", {
